cbb3a3cfba0acfb854eeb3a4e63ee18c87300bfe
windhooked <hannesdewaal@gmail.com>
2020-07-03 13:00:01 +0200

    change path

--- example/example.go
+++ example/example.go
@@ -5,8 +5,8 @@ import (
 	"log"
 	"time"
 
-	"github.com/hooklift/gowsdl/example/gen"
-	"github.com/hooklift/gowsdl/soap"
+	"github.com/windhooked/gowsdl/example/gen"
+	"github.com/windhooked/gowsdl/soap"
 )
 
 func ExampleBasicUsage() {
